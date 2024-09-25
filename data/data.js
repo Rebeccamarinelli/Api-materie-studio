c79c77c1622ece86a7f09ed6fbe585948630aaa9
Rebecca Marinelli <rebeccamarinelli18@gmail.com>
2024-09-25 13:45:19 +0200

    .

--- data/data.js
+++ data/data.js
@@ -3,7 +3,41 @@ const materie =[
         id: Date.now(),
         materia: "Typescript",
         domande: [
-            "Qual' è l'utilizzo di Typescript?"
+            "Cos'é Typescript?",
+            "Quali nuove funzionalità aggiunge?",
+            "Può essere eseguito da Node o Browser?",
+            "Cosa significa che Typescript viene compilato?",
+            "Perchè viene usato?",
+            "Dobbiamo installare Node?",
+            "Che significa Npm i -g Typescript?",
+            "Come si collega un file Typescript in Visual Studio Code?",
+            "tipi dinamici Vs. tipi statici?",
+        ]
+    },
+    {   
+        id: Date.now(),
+        materia: "Angular",
+        domande: [
+            "Cos'é Angular?",
+            "Cos'è un interceptor?",
+            "Cos'è una Guard?",
+            "Quali sono le direttive struturali in angular?",
+        ]
+    },
+    {   
+        id: Date.now(),
+        materia: "Javascript",
+        domande: [
+            "Cos'é Javascript?",
+            "Questo linguaggio è tipizzato?",
+        ]
+    },
+    {   
+        id: Date.now(),
+        materia: "React",
+        domande: [
+            "Cos'è React?",
+            "Su quale linguaggio si basa?",
         ]
     }
 
