846099d65608790e87e2130c52919ec3ec777682
Rebecca Marinelli <rebeccamarinelli18@gmail.com>
2024-09-27 18:56:14 +0200

    aggiunto cors

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ const port = 3000;
 // Middleware per gestire le richieste JSON
 app.use(express.json());
 
+const corsOptions = {
+  origin: 'http://localhost:4200', // Specifica l'origine permessa (es: il tuo frontend)
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Metodi permessi
+  allowedHeaders: ['Content-Type', 'Authorization'], // Header permessi
+  credentials: true // Permette l'invio di cookie
+};
+
+// Usa il middleware CORS con le opzioni specificate
+app.use(cors(corsOptions));
+
+
 // Una rotta di esempio: GET /users
 app.get('/', (req, res) => {
     res.json(materie);
